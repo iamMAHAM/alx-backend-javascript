dbb05fcbc8d1e43200e54ced120de4f1dddeb75d
iamMAHAM <abdulkabore@gmail.com>
2022-12-19 22:03:38 +0000

    fix loop error

--- 0x00-ES6_basic/10-loop.js
+++ 0x00-ES6_basic/10-loop.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const inter = [];
   for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+    inter.push(appendString + idx);
   }
 
-  return array;
+  return inter;
 }
