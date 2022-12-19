09576b4a4bf06112926550c65a809b7f6af982ac
iamMAHAM <abdulkabore@gmail.com>
2022-12-19 22:23:46 +0000

    using moderne es6 object notation

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,8 @@
 export default function createReportObject(employeesList) {
-  this.allEmployees = () => {
-    return { ...employeesList };
-  };
-  this.getNumberOfDepartments = () => {
-    return Object.keys(employeesList).length;
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
