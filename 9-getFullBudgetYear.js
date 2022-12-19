fbd110f2cafb600baa9de178bf1f2daaa2929cf1
iamMAHAM <abdulkabore@gmail.com>
2022-12-19 08:55:56 +0000

    9-getFull

---
+++ 9-getFullBudgetYear.js
@@ -0,0 +1,14 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  return {
+    ...budget,
+    getIncomeInDollars: function (income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros: function (income) {
+      return `${income} euros`;
+    },
+  };
+}
