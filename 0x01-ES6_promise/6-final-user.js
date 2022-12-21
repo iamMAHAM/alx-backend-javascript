e093f0b95760f197267a5680c178593bbd9a66f4
iamMAHAM <abdulkabore@gmail.com>
2022-12-21 09:47:46 +0000

    fix eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((pr) => ({
-      status: pr.status,
-      value: pr.status === 'fulfilled' ? pr.value : String(pr.reason),
-    }))
-  );
+  ]);
+  return results.map((pr) => ({
+    status: pr.status,
+    value: pr.status === 'fulfilled' ? pr.value : String(pr.reason),
+  }));
 }
 
 export default handleProfileSignup;
