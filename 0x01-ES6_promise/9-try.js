8617103fbe002aa04b7036a20845208dfa155557
iamMAHAM <abdulkabore@gmail.com>
2022-12-21 10:08:56 +0000

    fix eslint

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,10 @@
-import divideFunction from './8-try';
-
 export default function guardrail(mathFunction) {
   const queue = ['Guardrail was processed'];
   try {
     queue.unshift(mathFunction());
+    return queue;
   } catch (e) {
     queue.unshift(`Error: ${e.message}`);
-  } finally {
     return queue;
   }
 }
