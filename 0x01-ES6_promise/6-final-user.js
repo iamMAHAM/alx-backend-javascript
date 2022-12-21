6c4e29d322f0865d6b1e62af33db12a05a5e2fa8
iamMAHAM <abdulkabore@gmail.com>
2022-12-21 09:43:07 +0000

    fix error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,12 @@ function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((pr) => ({
+      status: pr.status,
+      value: pr.status === 'fulfilled' ? pr.value : String(pr.reason),
+    }))
+  );
 }
 
 export default handleProfileSignup;
