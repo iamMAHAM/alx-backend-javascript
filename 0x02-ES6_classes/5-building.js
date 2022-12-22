0448d31cbde85890b88a1a99516ed7a7cc5aa0a7
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 12:07:03 +0000

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft != 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new TypeError(
+      'Class extending Building must override evacuationWarningMessage'
+    );
+  }
+}
