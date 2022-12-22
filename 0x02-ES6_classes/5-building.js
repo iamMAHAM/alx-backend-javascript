d918c948d333fbd21aeca10d658682b61e9ee028
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 13:17:23 +0000

    fix eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -8,7 +8,7 @@ export default class Building {
     if (this.constructor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          'Class extending Building must override evacuationWarningMessage'
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
