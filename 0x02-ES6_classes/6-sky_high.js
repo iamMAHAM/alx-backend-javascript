ceac3775bf47869e9a5b2804f7a010c50e077865
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 14:48:05 +0000

    dynamize 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -17,6 +17,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   static evacuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
