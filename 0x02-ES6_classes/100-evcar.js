84330b5e93b0af74ad2a65c2cc139528dfd585a8
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 15:29:24 +0000

    100-ev car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+/* eslint no-underscore-dangle: 0 */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
