33dff5eec36c238b7bd766a77564bfb32f54ddb1
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 15:06:55 +0000

    car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(brand) {
+    this._brand = brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(motor) {
+    this._motor = motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
