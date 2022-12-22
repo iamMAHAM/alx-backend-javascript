100475d2f9b04c761ba7186c5c2a2f5b219b385f
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 12:45:44 +0000

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -38,4 +38,14 @@ export default class Current {
   displayFullCurrency() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('ConversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
 }
