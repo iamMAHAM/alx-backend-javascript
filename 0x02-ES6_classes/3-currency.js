71b2b80c89950655223386138494559e9a4075a4
iamMAHAM <abdulkabore@gmail.com>
2022-12-22 11:20:45 +0000

    currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Current {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw TypeError('Name or Code must be a string');
+    }
+    this._name = name;
+    this.code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._code = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
