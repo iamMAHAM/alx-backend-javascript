616a4015f308f5463faa4a6b6c382723d9074215
iamMAHAM <abdulkabore@gmail.com>
2023-01-02 12:23:58 +0000

    100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
 function createInt8TypedArray(length, position, value) {
-  const arrayBuffer = new ArrayBuffer(length);
-  const int8view = new Uint8Array(arrayBuffer);
-  int8view[position] = value;
-  return int8view;
+  if (position >= length) throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const intView = new Int8Array(buffer);
+  intView.set([value], position);
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
