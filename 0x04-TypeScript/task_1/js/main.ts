c82530084089932f5a0da2fb679ab1159a5a035c
iamMAHAM <abdulkabore@gmail.com>
2023-01-04 07:40:43 +0000

    teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,22 +1,8 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: number;
+  yearsOfExperience?: number;
   location: string;
+  [key: string]: any;
 }
-
-const s1: Student = {
-  firstName: 'paul',
-  lastName: 'alx',
-  age: 10,
-  location: 'Abidjan',
-};
-
-const s2: Student = {
-  firstName: 'paul1',
-  lastName: 'alx1',
-  age: 11,
-  location: 'Abidjan',
-};
-
-const studentList = [s1, s2];
