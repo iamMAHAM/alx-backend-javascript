eaa6f4fd39efd1dca3330b27a6f8a0467d04791b
iamMAHAM <abdulkabore@gmail.com>
2023-01-04 08:51:24 +0000

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,41 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+) {
+  return firstName.slice(0, 1) + `. ${lastName}`;
+};
+interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework: () => string;
+  displayName: () => string;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,67 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectoryTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getToWork() {
+    // i think this is a mistake for alx
+    return 'Getting a coffee break';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectoryTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: string | number): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) return new Teacher();
+  return new Director();
+}
+
+function isDirector(employee: unknown) {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: unknown) {
+  return isDirector(employee)
+    ? new Director().workDirectoryTasks()
+    : employee instanceof Teacher
+    ? new Teacher().workTeacherTasks()
+    : '';
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+  return todayClass === 'Math' ? 'Teaching Math' : 'Teaching History';
+}
